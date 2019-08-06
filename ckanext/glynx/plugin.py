34a0ae594d61949450170d2c5062a34cf9123423
Craig Stephenson <crstephenson@alaska.edu>
2019-08-06 06:36:28 +0000

    Add "Archived At" (date) and "Request Contact Info" (boolean) fields as examples.

--- ckanext/glynx/plugin.py
+++ ckanext/glynx/plugin.py
@@ -1,6 +1,7 @@
 import ckan.plugins as p
 import ckan.plugins.toolkit as tk
 
+# Vocabulary for custom Status dropdown is created here.
 def create_status_options():
     user = tk.get_action('get_site_user')({'ignore_auth': True}, {})
     context = {'user': user['name']}
@@ -28,18 +29,33 @@ class GlynxPlugin(p.SingletonPlugin, tk.DefaultDatasetForm):
     p.implements(p.IDatasetForm)
     p.implements(p.IConfigurer)
 
-    # Populate dropdowns on edit page with available options.
+    # Populates Status dropdown with vocabulary created above.
     def get_helpers(self):
         return {'status_options': status_options}
 
     def create_package_schema(self):
         schema = super(GlynxPlugin, self).create_package_schema()
 
-        # Custom "Status" field.
+        # Custom "Status" (dropdown) field.
         schema.update({
             'status': [
-                tk.get_validator('ignore_missing'),
                 tk.get_converter('convert_to_tags')('status_options')
+            ]
+        })
+
+        # Custom "Archived at" (date) field.
+        schema.update({
+            'archived_at': [
+                tk.get_validator('isodate'),
+                tk.get_converter('convert_to_extras')
+            ]
+        })
+
+        # Custom "Request Contact Info" (boolean) field.
+        schema.update({
+            'request_contact_info': [
+                tk.get_validator('boolean_validator'),
+                tk.get_converter('convert_to_extras')
             ]
         })
 
@@ -48,11 +64,26 @@ class GlynxPlugin(p.SingletonPlugin, tk.DefaultDatasetForm):
     def update_package_schema(self):
         schema = super(GlynxPlugin, self).update_package_schema()
 
-        # Custom "Status" field.
+        # Custom "Status" (dropdown) field.
         schema.update({
             'status': [
-                tk.get_validator('ignore_missing'),
                 tk.get_converter('convert_to_tags')('status_options')
+            ]
+        })
+
+        # Custom "Archived at" (date) field.
+        schema.update({
+            'archived_at': [
+                tk.get_validator('isodate'),
+                tk.get_converter('convert_to_extras')
+            ]
+        })
+
+        # Custom "Request Contact Info" (boolean) field.
+        schema.update({
+            'request_contact_info': [
+                tk.get_validator('boolean_validator'),
+                tk.get_converter('convert_to_extras')
             ]
         })
 
@@ -62,12 +93,28 @@ class GlynxPlugin(p.SingletonPlugin, tk.DefaultDatasetForm):
         schema = super(GlynxPlugin, self).show_package_schema()
         schema['tags']['__extras'].append(tk.get_converter('free_tags_only'))
 
-        # Custom "Status" field.
+        # Custom "Status" (dropdown) field.
         schema.update({
             'status': [
-                tk.get_converter('convert_from_tags')('status_options'),
-                tk.get_validator('ignore_missing')]
-            })
+                tk.get_converter('convert_from_tags')('status_options')
+            ]
+        })
+
+        # Custom "Archived at" (date) field.
+        schema.update({
+            'archived_at': [
+                tk.get_validator('isodate'),
+                tk.get_converter('convert_from_extras')
+            ]
+        })
+
+        # Custom "Request Contact Info" (boolean) field.
+        schema.update({
+            'request_contact_info': [
+                tk.get_validator('boolean_validator'),
+                tk.get_converter('convert_from_extras')
+            ]
+        })
 
         return schema
 
